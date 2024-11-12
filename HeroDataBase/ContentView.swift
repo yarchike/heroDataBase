c67a9cd68fa7083125a88560818df980c5895cea
yarchike <yarchike@gmail.com>
2024-11-12 11:30:28 +0300

    Initial Commit

---
+++ HeroDataBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeroDataBase
+//
+//  Created by Ярослав  Мартынов on 12.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeroDataBase/HeroDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroDataBaseApp.swift
+//  HeroDataBase
+//
+//  Created by Ярослав  Мартынов on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeroDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
