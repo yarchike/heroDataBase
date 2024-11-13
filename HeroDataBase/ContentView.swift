3b3028d3de601c2e5b108024efc6231472b208c0
yarchike <yarchike@gmail.com>
2024-11-13 11:15:05 +0300

    work 2

--- HeroDataBase/ContentView.swift
+++ HeroDataBase/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            InfoView()
+            InfoView(titleOn: titleOn)
                 .tabItem {
                     Label("Info", systemImage: "book")
                 }
