a0a55eac51e7481c63bb3121a9f686c6dd46e63e
yarchike <yarchike@gmail.com>
2024-11-14 11:14:33 +0300

    work 1

--- HeroDataBase/HelloView.swift
+++ HeroDataBase/HelloView.swift
@@ -9,11 +9,28 @@ import Foundation
 import SwiftUI
 
 struct HelloView: View {
-    var body: some View {
-        Text("Hello world")
-            .font(.largeTitle)
-            .padding()
-    }
+
+    @State private var isVisible = false
+
+      var body: some View {
+          VStack {
+              Image(systemName: "hand.wave")
+                  .font(.largeTitle)
+                  .scaleEffect(isVisible ? 1.5 : 1.0)
+                  .onAppear {
+                      withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
+                          isVisible.toggle()
+                      }
+                  }
+              
+              Text("Добро пожаловать в ваше приложение!")
+                  .font(.headline)
+                  .padding()
+          }
+      }
+    
+    
+    
 }
 
 
